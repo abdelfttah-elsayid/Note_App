ddaf9801f31e985fb9fedefe1e0247eb6e6bf031
abdelfttah-elsayid <75938804+abdelfttah-elsayid@users.noreply.github.com>
2026-03-28 07:11:35 +0200

    first

---
+++ lib/main.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/cupertino.dart';
+
+void main(){
+  runApp(const NoteApp());
+}
+class NoteApp extends StatelessWidget {
+  const NoteApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
