98d5ec5fa319873479cf0e209d71ca0559dd18cf
abdelfttah-elsayid <75938804+abdelfttah-elsayid@users.noreply.github.com>
2026-03-29 15:09:05 +0200

    first

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 import 'package:note_app/constants/constants.dart';
+import 'package:note_app/cubits/add_notes_cubit/add_note_cubit.dart';
 import 'package:note_app/models/note_model.dart';
 import 'package:note_app/screens/home_screen.dart';
 
@@ -16,12 +18,17 @@ class NoteApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-        theme: ThemeData(
-          brightness: Brightness.dark,
-        ),
-        home: const HomeScreen(),
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider(create: (context) => AddNoteCubit())
+      ],
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+          theme: ThemeData(
+            brightness: Brightness.dark,
+          ),
+          home: const HomeScreen(),
+      ),
     );
 
   }
